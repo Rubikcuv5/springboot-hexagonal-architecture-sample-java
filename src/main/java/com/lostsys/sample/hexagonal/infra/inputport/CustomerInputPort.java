479672f443dfead069395a4e53b010e345736777
EmmanuelCruzL <cruz.emma.1gm@hotmail.com>
2023-06-20 12:38:42 -0600

    add java spring project

---
+++ src/main/java/com/lostsys/sample/hexagonal/infra/inputport/CustomerInputPort.java
@@ -0,0 +1,14 @@
+package com.lostsys.sample.hexagonal.infra.inputport;
+
+import java.util.List;
+
+import com.lostsys.sample.hexagonal.domain.Customer;
+
+public interface CustomerInputPort {
+    
+    public Customer createCustomer(String name,String Conutry);
+
+    public Customer getById(String customerId);
+
+    public List<Customer> getAll();
+}
